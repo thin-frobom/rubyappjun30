9a7eddfbbdc917213a1f852115165925628cbb47
thin <thin.thin.aung@frobom.com>
2017-06-30 16:54:14 +0630

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+   get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#hello'
+  
+  get '/signup', to: 'users#new'
+
+  post '/signup', to: 'users#create'
+  resources :users
+end
